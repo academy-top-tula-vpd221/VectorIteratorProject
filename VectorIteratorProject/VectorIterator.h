a0b259300f4216a7e6eaa3149a3e24d03d1b955d
Ефимов Максим <efimov_m@itstep.academy>
2022-12-04 17:51:31 +0300

    04-12-22

--- VectorIteratorProject/VectorIterator.h
+++ VectorIteratorProject/VectorIterator.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "Vector.h"
-
 template <class T>
 class Iterator
 {
--- VectorIteratorProject/VectorIteratorProject.cpp
+++ VectorIteratorProject/VectorIteratorProject.cpp
@@ -14,39 +14,39 @@ void ContainerPrint(Iterator<T>* it)
 
 int main()
 {
-    srand(time(nullptr));
+    //srand(time(nullptr));
 
-    Vector<int> v1(10);
+    //Vector<int> v1(10);
 
 
-    cout << v1.Size() << " " << v1.Capacity() << "\n";
+    //cout << v1.Size() << " " << v1.Capacity() << "\n";
 
-    Iterator<int>* it1 = v1.GetBeginIt();
+    //Iterator<int>* it1 = v1.GetBeginIt();
 
-    for (int i = 0; i < v1.Size(); i++)
-        v1[i] = rand() % 100;
+    //for (int i = 0; i < v1.Size(); i++)
+    //    v1[i] = rand() % 100;
 
-    /*for (int i = 0; i < v1.Size(); i++)
-        cout << v1[i] << " ";*/
+    ///*for (int i = 0; i < v1.Size(); i++)
+    //    cout << v1[i] << " ";*/
 
-    ContainerPrint(it1);
+    //ContainerPrint(it1);
 
-    Vector<double> v2(5);
-    for (int i = 0; i < v2.Size(); i++)
-        v2[i] = (rand() % 100) / 10.5;
+    //Vector<double> v2(5);
+    //for (int i = 0; i < v2.Size(); i++)
+    //    v2[i] = (rand() % 100) / 10.5;
 
-    cout << "\n";
+    //cout << "\n";
 
-    Iterator<double>* it2 = v2.GetBeginIt();
-    ContainerPrint(it2);
+    //Iterator<double>* it2 = v2.GetBeginIt();
+    //ContainerPrint(it2);
 
-    cout << "\n";
+    //cout << "\n";
 
-    ForwardList<int> list;
-    for (int i = 0; i < 8; i++)
-        list.Insert(0, rand() % 100);
+    //ForwardList<int> list;
+    //for (int i = 0; i < 8; i++)
+    //    list.Insert(0, rand() % 100);
 
-    Iterator<int>* lit1 = list.GetBeginIt();
-    ContainerPrint(lit1);
+    //Iterator<int>* lit1 = list.GetBeginIt();
+    //ContainerPrint(lit1);
     
 }
